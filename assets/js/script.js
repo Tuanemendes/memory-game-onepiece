d54c6f5445af82b4bcc2a94c4ad2e2424249b869
Tuanemendes <tuane.belmiro@gmail.com>
2023-05-26 16:24:28 -0300

    transição das cartas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,16 @@
 
 function redirectGame() {
     window.location.href = "memory-game.html";
-    
+
 }
 
 
+
+const cards = document.querySelectorAll('.c-card');
+
+function flipCard() {
+    console.log('I was clicked');
+    this.classList.toggle('flip');
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
